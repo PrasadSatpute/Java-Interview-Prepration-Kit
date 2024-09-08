a3f320c8e9830e22d547deea48cbef78a6a46f2e
PRASAD PURUSHOTTAM SATPUTE <32232749+PrasadSatpute@users.noreply.github.com>
2024-09-08 14:54:28 +0530

    Update

--- JAVA 8/Code/Java8/src/main/java/org/example/lambda/Test1.java
+++ JAVA 8/Code/Java8/src/main/java/org/example/lambda/Test1.java
@@ -1,14 +1,36 @@
 package org.example.lambda;
 
+interface AnonymousInnerClass
+{
+    public void sayHi();
+    public void sayBy();
+}
+
 @FunctionalInterface
 interface MyInterface
 {
     public void sayHi();
 }
+// Anonymous Inner Class and Lambda Expression
+
 public class Test1 {
     public static void main(String[] args) {
+        AnonymousInnerClass anonymousInnerClass = new AnonymousInnerClass() {
+            @Override
+            public void sayHi() {
+                System.out.println("AnonymousInnerClass Hi");
+            }
+
+            @Override
+            public void sayBy() {
+                System.out.println("AnonymousInnerClass Bye");
+            }
+        };
+        anonymousInnerClass.sayHi();
+        anonymousInnerClass.sayBy();
+
         MyInterface myInterface = () -> {
-            System.out.println("Hi Lambda Expression");
+            System.out.println("Lambda Expression Hi");
         };
         myInterface.sayHi();
     }
